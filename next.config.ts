b692bf1d2d4fef0b17b08d713080a2e9b50b17ea
meisterjustice <eziefulejustice@gmail.com>
2025-01-05 16:47:39 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.imgur.com",
       },
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+      },
     ],
   },
 };
