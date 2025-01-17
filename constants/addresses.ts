0b881a0d7eb35b594af50767c9d6d7fd70866097
meisterjustice <eziefulejustice@gmail.com>
2025-01-17 19:42:29 +0100

    update addresses

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -2,13 +2,13 @@ export const USDCContractAddress =
   "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913" as `0x${string}`;
 
 export const engineContractAddress =
-  "0xcB7198EF5AdAE20083692bBc71c561456A536705" as `0x${string}`;
+  "0x68b57A15B43D3342d2a2680447c8525522c1dE6F" as `0x${string}`;
 
 export const strategyContractAddress =
-  "0x681510010e7A44869a51135C01ee4B0071a91ec3" as `0x${string}`;
+  "0x40D5f2a449FeA5b12C52dD16CbA5698161C18916" as `0x${string}`;
 
 export const moonwellConnector =
-  "0x471A3023E640f5acF2cd4cDa574a13918303510d" as `0x${string}`;
+  "0xef04d3c09332575FC14c371658DE78028Ab6faE9" as `0x${string}`;
 
 export const morphoConnector =
-  "0x61cfeA4cCC27ae5dD11D4121f5d48229adebDa52" as `0x${string}`;
+  "0x6075bf178792b456aa063cC5871A144001d367B1" as `0x${string}`;
