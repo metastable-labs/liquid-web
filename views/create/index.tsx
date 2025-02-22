b7dc0009e4ac43824428bdf01be084dc9981a454
meisterjustice <eziefulejustice@gmail.com>
2025-02-22 15:13:02 +0100

    update agent component

--- views/create/index.tsx
+++ views/create/index.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import Link from "next/link";
 
-import { LWAgentCard } from "@/components";
-import { dummyAgents } from "./dummy";
+import ExploreAgents from "./explore-agents";
 
 const Create = () => {
   return (
@@ -22,19 +20,7 @@ const Create = () => {
         </p>
       </div>
 
-      <div className="flex flex-col items-stretch gap-6 lg:self-center self-stretch">
-        <h1 className="text-[15px] leading-[19.8px] font-medium text-primary-1700">
-          Explore Agents from the community
-        </h1>
-
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-2 lg:grid-cols-3 max-w-[1077px]">
-          {dummyAgents.map((agent, index) => (
-            <Link href={`/${agent?.id}/trade`} key={index}>
-              <LWAgentCard agent={agent} variant="secondary" />
-            </Link>
-          ))}
-        </div>
-      </div>
+      <ExploreAgents />
     </div>
   );
 };
